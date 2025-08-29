d62f629248b69f7af176bc5f12add3fe5b3cee3a
erikG <work.erikgavrilov@gmail.com>
2025-08-29 19:47:50 +0300

    add notification

--- utils/toggle-nightlight.sh
+++ utils/toggle-nightlight.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ "$(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled)" = "true" ]; then
     gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
+    notify-send "Night Light" "Night Light turned OFF"
 else
     gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+    notify-send "Night Light" "Night Light turned ON"
 fi
 
