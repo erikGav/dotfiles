7b0a8384f7d09c9eca7f2beb070524e1c9e26d95
erikG <work.erikgavrilov@gmail.com>
2025-12-22 23:10:15 +0200

    toggle night light i3+gnome

--- utils/toggle-nightlight.sh
+++ utils/toggle-nightlight.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-if [ "$(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled)" = "true" ]; then
-    gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
-    notify-send "Night Light" "Night Light turned OFF"
+
+is_gnome() {
+    echo "$XDG_CURRENT_DESKTOP $DESKTOP_SESSION $GDMSESSION" | grep -qi gnome
+}
+
+if is_gnome; then
+    SESSION="GNOME"
+    if [ "$(gsettings get org.gnome.settings-daemon.plugins.color night-light-enabled)" = "true" ]; then
+        gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
+        notify-send -t 1300 "Night Light ($SESSION)" "Night Light turned OFF"
+    else
+        gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+        notify-send -t 1300 "Night Light ($SESSION)" "Night Light turned ON"
+    fi
 else
-    gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-    notify-send "Night Light" "Night Light turned ON"
+    SESSION="i3"
+    if pgrep -x gammastep >/dev/null; then
+        gammastep -x
+        pkill gammastep
+        notify-send -t 1300 "Night Light ($SESSION)" "Night Light turned OFF"
+    else
+        # Run detached so it doesn't block the terminal
+        nohup gammastep >/dev/null 2>&1 &
+        notify-send -t 1300 "Night Light ($SESSION)" "Night Light turned ON"
+    fi
 fi
 
