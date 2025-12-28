cf5d62a46f4852283c2e40d354d94ced91978fb4
erikG <work.erikgavrilov@gmail.com>
2025-12-28 15:18:43 +0200

    update dot files

--- utils/browser.sh
+++ utils/browser.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-BROWSER="brave-browser"
-options=" Google
- YouTube
-󰃭 Calendar"
 
-choice=$(echo -e "$options" | rofi -dmenu \
+BROWSER="brave-browser"
+
+declare -A sites=(
+    [" Google"]="https://www.google.com"
+    [" YouTube"]="https://www.youtube.com"
+    ["󰃭 Calendar"]="https://calendar.google.com"
+)
+
+options=$(printf "%s\n" "${!sites[@]}")
+
+choice=$(echo -e "$options" | rofi -dmenu -i \
   -p "Search or open site")
 
 [[ -z "$choice" ]] && exit 0
 
-# Check if choice matches any of the menu options
-case "$choice" in
-  *Google)   exec $BROWSER "https://www.google.com" ;;
-  *YouTube)  exec $BROWSER "https://www.youtube.com" ;;
-  *Calendar) exec $BROWSER "https://calendar.google.com" ;;
-  *)
-    # Anything else is a search query
+# Check if choice matches any site
+if [[ -v sites["$choice"] ]]; then
+    exec $BROWSER "${sites[$choice]}"
+else
+    # No match - treat as search query
     query=$(printf '%s' "$choice" | sed 's/ /+/g')
     exec $BROWSER "https://www.google.com/search?q=$query"
-    ;;
-esac
+fi
